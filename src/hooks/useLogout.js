df8842d7baf7dc9881c354e48a3b33c5ffac8571
minugrammer0725 <kwawkminoo@gmail.com>
2022-04-24 12:19:30 -0700

    clean up function for custom hooks using useEffect

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const [isCancelled, setIsCancelled] = useState(false);
   const { dispatch } = useAuthContext();
 
   const logout = async () => {
@@ -15,14 +16,24 @@ export const useLogout = () => {
       await projectAuth.signOut();
       // dispatch() LOGOUT action
       dispatch({ type: "LOGOUT" });
-      setIsPending(false);
-      setError(null);
+      // check "cancelled" before updating states.
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (error) {
-      console.log(error.message);
-      setError(error.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.log(error.message);
+        setError(error.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    // clean up function.
+    return () => setIsCancelled(true);
+  }, []);
 
   return { error, isPending, logout };
 };
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const [isCancelled, setIsCancelled] = useState(false);
   const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
@@ -30,15 +31,23 @@ export const useSignup = () => {
 
       // dispatch() LOGIN action.
       dispatch({ type: "LOGIN", payload: res.user });
-
-      setIsPending(false);
-      setError(null);
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (error) {
-      console.log(error.message);
-      setError(error.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.log(error.message);
+        setError(error.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    // clean up function.
+    return () => setIsCancelled(true);
+  }, []);
 
   return { error, isPending, signup };
 };
