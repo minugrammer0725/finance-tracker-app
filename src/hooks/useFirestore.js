4e17dbb031cd5427a2ebe1a124e3799662698eff
minugrammer0725 <kwawkminoo@gmail.com>
2022-04-25 10:38:21 -0700

    Adding document to firebase using form

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useReducer } from "react";
-import { projectFirestore } from "../firebase/config";
+import { projectFirestore, timestamp } from "../firebase/config";
 
 let initialState = {
   document: null,
@@ -10,6 +10,22 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDING":
+      return { isPending: true, document: null, success: false, error: null };
+    case "ADD_DOC":
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -23,11 +39,31 @@ export const useFirestore = (collection) => {
   // grab the collection we need
   const ref = projectFirestore.collection(collection);
 
+  // helper function
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   // add document
-  const addDoc = (doc) => {};
+  const addDoc = async (doc) => {
+    dispatch({
+      type: "IS_PENDING",
+    });
+
+    try {
+      const createdAt = timestamp.fromDate(new Date());
+      const addedDoc = await ref.add({ ...doc, createdAt });
+      dispatchIfNotCancelled({ type: "ADD_DOC", payload: addedDoc });
+    } catch (error) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: error.message });
+    }
+  };
 
   // delete document
-  const deleteDoc = (id) => {};
+  const deleteDoc = async (id) => {};
 
   useEffect(() => {
     return () => setIsCancelled(true);
--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 
-const TransactionForm = () => {
+const TransactionForm = ({ uid }) => {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const { addDoc, response } = useFirestore("transactions");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // save transaction to firestore database.
-    console.log(name, amount);
+    console.log(uid, name, amount);
+    addDoc({ uid, name, amount });
   };
+
+  // clear form onSubmit.
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
 
   return (
     <>
